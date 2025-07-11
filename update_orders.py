7a68bc0389be49165f02a911e5bbbf6c03395ec9
dchurchwell <dchurchwell@mines.edu>
2025-07-10 21:35:51 -0600

    update

--- update_orders.py
+++ update_orders.py
@@ -201,7 +201,7 @@ def order_fulfillments():
             for profession in banker_record:
                 num = banker_record[profession]
                 pts = num / 4
-                f.write(f"{user} donated {num} {profession} items from the guild bank {pts} s\n")
+                f.write(f"\n{user} donated {num} {profession} items from the guild bank {pts} s\n")
 
                 channel_id = channel_ids[profession]
                 post = (channel_id, f"{user} earned {pts} points!")
